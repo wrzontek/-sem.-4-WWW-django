284f633867a6b50c50507b333b6176aac7fd7db2
Wrzontek <matwiejuk.adrian@gmail.com>
2021-05-14 20:31:58 +0200

    testy teściki

--- framaw/tests.py
+++ framaw/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import *
+from .views import *
 
 
 class ModelTests(TestCase):
@@ -37,3 +39,58 @@ class ModelTests(TestCase):
         self.assertEqual(len(SectionStatus.objects.all()), 0)
         self.assertEqual(len(StatusData.objects.all()), 0)
         self.assertEqual(len(User.objects.all()), 1)
+
+
+class ViewsTests(TestCase):
+
+    def test_no_logon(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logon_invalid(self):
+        c = Client()
+        response = c.post('/framaw/', {'username': 'john', 'password': 'smith'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"invalid username or password")
+
+    def test_logon_valid(self):
+        c = Client()
+        User.objects.create_user(username="usr", password="password123").save()
+        response = c.post('/framaw/', {'username': 'usr', 'password': 'password123'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_new_file(self):
+        c = Client()
+        User.objects.create_user(username="usr", password="password123").save()
+        response = c.post('/framaw/', {'username': 'usr', 'password': 'password123'})
+        self.assertEqual(response.status_code, 302)
+        response = c.post('/framaw/new_file/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_describe_section(self):
+        user = User(username="test_usr", password="examplepassword123124$")
+        dir = Directory(name="test_dir", description="test_desc", owner=user)
+        file = File(name="test_file", description="test_desc", owner=dir.owner, directory=dir, content="łubudubu")
+
+        user.save()
+        dir.save()
+        file.save()
+
+        fs = FileSection(file=file, line_number=0,
+                         content="  @   ensures \\result >= 0 ==> t[\\result] == v;\r\n  @ behavior failure:\r\n")
+        fs.save()
+
+        describe_section(fs, user)
+
+        self.assertEqual(len(FileSection.objects.all()), 1)
+        self.assertEqual(len(SectionCategory.objects.all()), 1)
+        self.assertEqual(len(SectionStatus.objects.all()), 1)
+        self.assertEqual(len(StatusData.objects.all()), 1)
+
+        sc = SectionCategory.objects.all()[0]
+        ss = SectionStatus.objects.all()[0]
+        sd = StatusData.objects.all()[0]
+
+        self.assertEqual(sc.category, "ensures")
+        self.assertEqual(ss.status, "unchecked")
+        self.assertEqual(sd.data, "")
